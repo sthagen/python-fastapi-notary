7a1c4fe642298dc5e1a8333dd788ad8470584414
Stefan Hagen <stefan@hagen.link>
2020-12-03 13:04:24 +0100

    Update test_ping.py

--- src/tests/test_ping.py
+++ src/tests/test_ping.py
@@ -1,4 +1,4 @@
 def test_ping(test_app):
-    response = test_app.get("/ping")
+    response = test_app.get("/notary/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
